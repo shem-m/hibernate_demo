ee80efe5d851083bd041993e82f8c4de055f8676
shem-m <65406861+shem-m@users.noreply.github.com>
2020-12-11 01:07:21 +0300

    All problems fixed

--- src/main/java/dao/UserDAOImpl.java
+++ src/main/java/dao/UserDAOImpl.java
@@ -5,19 +5,14 @@ import models.User;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import utils.HibernateSessionFactoryUtil;
-
 import java.util.List;
 
-public class UserDAOImpl implements UserDAO {
-    @Override
+public class UserDAOImpl {
+
     public User findById(int id) {
-        Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
-        User user = session.get(User.class, id);
-        session.close();
-        return user;
+        return HibernateSessionFactoryUtil.getSessionFactory().openSession().get(User.class, id);
     }
 
-    @Override
     public void save(User user) {
         Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
         Transaction tx1 = session.beginTransaction();
@@ -26,7 +21,6 @@ public class UserDAOImpl implements UserDAO {
         session.close();
     }
 
-    @Override
     public void update(User user) {
         Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
         Transaction tx1 = session.beginTransaction();
@@ -35,7 +29,6 @@ public class UserDAOImpl implements UserDAO {
         session.close();
     }
 
-    @Override
     public void delete(User user) {
         Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
         Transaction tx1 = session.beginTransaction();
@@ -44,19 +37,13 @@ public class UserDAOImpl implements UserDAO {
         session.close();
     }
 
-    @Override
     public Auto findAutoById(int id) {
-        Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
-        Auto auto = session.get(Auto.class, id);
-        session.close();
-        return auto;
+        return HibernateSessionFactoryUtil.getSessionFactory().openSession().get(Auto.class, id);
     }
 
-    @Override
     public List<User> findAll() {
-        Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
-        List<User> users = (List<User>) session.createQuery("FROM " + User.class.getName()).list();
-        session.close();
+        List<User> users = (List<User>)  HibernateSessionFactoryUtil
+                .getSessionFactory().openSession().createQuery("From User").list();
         return users;
     }
 }
